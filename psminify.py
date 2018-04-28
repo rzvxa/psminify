39a67ac54781f54c7bcab3f98da016f4d9300a11
Ali Rezvani <3788964+aliagamon@users.noreply.github.com>
2018-04-28 08:17:46 +0430

    Added argument parser
    now it uses argparse for argument instead of sys.args

--- psminify.py
+++ psminify.py
@@ -2,25 +2,26 @@ import sys
 import os
 import re
 import ntpath as ntp
-fname = sys.argv[1]
-try:
-    fout = sys.argv[2]
-except IndexError:
-    fout = None
-with open(fname) as f:
-    content = f.readlines()
-# you may also want to remove whitespace characters like `\n` at the end of each line
+import argparse
+parser = argparse.ArgumentParser(description='PowerShell Minifier')
+parser.add_argument('fin', metavar='input file', type=file, nargs=1, help='input PowerShell source')
+parser.add_argument('--fout', metavar='output file', type=argparse.FileType('w+'), nargs='?', help='output file of minified version')
+args = parser.parse_args()
+fin = args.fin[0]
+fout = args.fout
+content = fin.readlines()
 content = [x.strip() for x in content]
 result = ''
 lastToken = None
+# remove all whitespaces and just add ; where ever needed
 for token in content:
     token = re.sub('\s\s+(".*?"|.*?)*?', " ", token)
     if lastToken is not None and lastToken != '{' and lastToken != '}' and token != '{' and token != '}':
         result += ';'
     result += token
     lastToken = token
+# if there is no output path just save output in same directory with the name format like this [orginal_name].min.[format]
 if fout is None:
-    basenameObj =  os.path.splitext(ntp.basename(fname))
-    fout = basenameObj[0] + '.min' + basenameObj[1]
-with open(fout,"w+") as f:
-    f.write(result)
+    basenameObj =  os.path.splitext(ntp.basename(fin.name))
+    fout = open(basenameObj[0] + '.min' + basenameObj[1], "w+")
+fout.write(result)
